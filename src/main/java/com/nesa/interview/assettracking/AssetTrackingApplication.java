2525835f55cfb5690766631e00446f2c96ffbc1d
Faizal Rahmah <reachfaizal08@gmail.com>
2026-03-09 16:42:16 +0530

    1st commit: asset tracking project structure and configuration

---
+++ src/main/java/com/nesa/interview/assettracking/AssetTrackingApplication.java
@@ -0,0 +1,12 @@
+package com.nesa.interview.assettracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssetTrackingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssetTrackingApplication.class, args);
+    }
+}
